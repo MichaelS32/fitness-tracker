ced557c1d646fff9d6e50678d20056b483e70946
BIGCHAVO7810 <jamestchavez@gmail.com>
2022-09-28 21:06:52 -0500

    added password hash function to user.js

--- server/models/user.js
+++ server/models/user.js
@@ -23,4 +23,12 @@ const UserSchema = new Schema({
         type: Date,
         default: Date.now,
     },
-})
+});
+
+UserSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
