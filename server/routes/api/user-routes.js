3eef1cff54f9fe1319651380df4666bc2693f04f
BIGCHAVO7810 <jamestchavez@gmail.com>
2022-09-29 18:58:42 -0500

    added some of the routes on user-routes.js

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,1 +1,22 @@
 const router = require("express").Router();
+const {
+    createUser,
+    getSingleUser,
+    login,
+    createCardio,
+    createResistance,
+    getAllCardio,
+    getCardioById,
+    getAllResistance,
+    getResistanceById,
+    createGoals,
+    getAllGoals,
+} = require('../../controllers/usercontroller');
+
+const { authMiddleware } = require('../../utils/auth');
+
+router.route('/').post(createUser).put(authMiddleware);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
