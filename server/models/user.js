898df9640166dcb9fae58675b41f68fcd27bc782
BIGCHAVO7810 <jamestchavez@gmail.com>
2022-09-28 20:59:48 -0500

    added UserSchema to user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,26 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: "Username Required"
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: "Password Required",
+        minlength: 8,
+    },
+    email: {
+        type: String,
+        unique: true,
+        match: [/.+@.+\..+/],
+    },
+    userCreated: {
+        type: Date,
+        default: Date.now,
+    },
+})
