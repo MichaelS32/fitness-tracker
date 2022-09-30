b7761f6b00e3d1b4ce0406c6188f09b45a14a8e1
BIGCHAVO7810 <jamestchavez@gmail.com>
2022-09-29 19:10:21 -0500

    finished user-routes.js

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -20,3 +20,13 @@ router.route('/').post(createUser).put(authMiddleware);
 router.route('/login').post(login);
 
 router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/cardio').post(createCardio).get(getAllCardio);
+router.route('/cardio/:id').get(getCardioById);
+
+router.route('/resistance').post(createResistance).get(getAllResistance);
+router.route('/resistance/"id').get(getResistanceById);
+
+router.route('/goal').post(createGoals).get(getAllGoals);
+
+module.exports = router;
