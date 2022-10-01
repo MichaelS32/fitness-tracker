2bbe9b8d9e6d8e820b1bc190a2b876be2f904a2c
Gunnar Smith <gunnarsmith3@gmail.com>
2022-10-01 12:39:05 -0500

    homepage

---
+++ client/src/components/home-containers/hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/hero.css";
+
+function Hero() {
+    return (
+        <section className="hero">
+        <div className="hero-cta">
+            <h2>FIND THE RIGHT WORKOUTS FOR THE RIGHT GAINS</h2>
+            <p>Track your own workouts to keep the right plan for you and find new creative workout plans to add to your training</p>
+        </div>
+        </section>
+    );
+}
+
+export default Hero;
